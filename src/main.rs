b35fdfe4f1d2502b44b6a58222b64d0459590210
Alexander Bliskovsky <abliskovsky@fusionbox.com>
2017-04-21 15:10:03 -0700

    Allow more than one command on a line

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,16 @@ fn main() {
         io::stdin().read_line(&mut input)
             .expect("Failed to read line.");
 
-        let input = parse_input(&input);
+        let words = input.split_whitespace();
 
-        match input {
-            InputToken::Number(num) => stack.push(num),
-            InputToken::Command(c) => handle_command(c, &stack),
-            InputToken::Operator(o) => handle_operator(o, &mut stack),
+        for w in words {
+            let input = parse_input(w);
+
+            match input {
+                InputToken::Number(num) => stack.push(num),
+                InputToken::Command(c) => handle_command(c, &stack),
+                InputToken::Operator(o) => handle_operator(o, &mut stack),
+            }
         }
     }
 }
@@ -37,7 +41,7 @@ fn handle_operator(op: char, stack: &mut Vec<i64>) {
 
 fn handle_command(comm: char, stack: &Vec<i64>) {
     match comm {
-        'p' => {
+        'f' => {
             for i in stack {
                 println!("{}", i);
             }
@@ -47,13 +51,17 @@ fn handle_command(comm: char, stack: &Vec<i64>) {
 }
 
 fn parse_input(input: &str) -> InputToken {
-    match input.trim().parse::<i64>() {
-        Ok(num) => InputToken::Number(num),
-        Err(_) => match input.chars().next() {
+    let input = input.trim();
+    let parse_result = input.parse::<i64>();
+
+    if let Ok(num) = parse_result {
+        return InputToken::Number(num);
+    }
+
+    match input.chars().next() {
             Some('+') => InputToken::Operator('+'),
             Some('-') => InputToken::Operator('-'),
-            Some('p') => InputToken::Command('p'),
+            Some('f') => InputToken::Command('f'),
             _ => panic!("Invalid input"),
-        },
     }
 }
